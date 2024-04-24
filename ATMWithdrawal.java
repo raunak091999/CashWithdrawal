56d79cd9c9685d28462918b32b5bca065517af84
Singh_ Raunak (raunsingh@deloitte.com) <raunsingh@your.hostname.com>
2024-04-24 20:09:12 +0530

    used concurrent hashmap

--- ATMWithdrawal.java
+++ ATMWithdrawal.java
@@ -4,77 +4,69 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 public class ATMWithdrawal {
+    private final Logger logger = Logger.getLogger(ATMWithdrawal.class.getName());
     private double balance;
     private Lock lock;
 
     Map<Integer, Integer> notes = new ConcurrentHashMap<Integer, Integer>();
+    int[] denominations = new int[]{2000, 500, 200, 100, 50, 20, 10};
 
     public ATMWithdrawal() {
         lock = new ReentrantLock();
-        notes.put(2000,20);
-        notes.put(1000,20);
-        notes.put(500,20);
-        notes.put(200,20);
-        notes.put(100,20);
-        notes.put(20,20);
-        notes.put(10,20);
+
+        notes.put(2000, 20);
+        notes.put(1000, 20);
+        notes.put(500, 20);
+        notes.put(200, 20);
+        notes.put(100, 20);
+        notes.put(50, 20);
+        notes.put(20, 20);
+        notes.put(10, 20);
     }
 
+    public int validationBalance(int amount, int balance) {
+        return amount % 10 != 0 ? 0 : 1;
 
-    public void withdraw(int amount, int balance) {
+    }
+
+    /**
+     * To withdraw minimum number of notes
+     * @param amount - entered by user
+     * @param balance- avialable balance
+     */
+    public int withdraw(int amount, int balance) {
         lock.lock();
         try {
-            if(amount%10!=0)
-            {
-                System.out.println("Please input amount in multiple of 10");
-            }
-            else if (balance >= amount) {
-
-                int demand=amount;
-                int[] notes = new int[]{ 2000, 500, 200, 100, 50, 20, 10 };
-                int[] noteCounter = new int[7];
-                Iterator<Map.Entry<Integer, Integer>> iterator = notes.entrySet().iterator();
-                while (iterator.hasNext()) {
-                    Map.Entry<Integer, Integer> entry = iterator.next();
-                    System.out.println("Note: " + entry.getKey() + ", Quantity: " + entry.getValue());
-                }
-                // count notes using Greedy approach
-                for (int i = 0; i < 7; i++) {
-                    if (amount >= notes[i]) {
-                        noteCounter[i] = amount / notes[i];
-                        amount = amount % notes[i];
+            if (validationBalance(amount, balance) == 0) {
+                logger.severe("Please input amount in multiple of 10");
+            } else if (balance >= amount) {
+                for (int denomination : denominations) {
+                    if (amount >= denomination) {
+                        int leftNotes = notes.get(denomination);
+                        int numberOfNotes = amount / notes.get(denomination);
+                        notes.replace(denomination, leftNotes - numberOfNotes);
+                        amount = amount % denomination;
                     }
                 }
-
-                // Print notes
-                System.out.println("Currency Count ->");
-                for (int i = 0; i < 7; i++) {
-                    if (noteCounter[i] != 0) {
-                        System.out.println(notes[i] + " : "
-                                + noteCounter[i]);
-                    }
-                }
-
-
-                balance -= amount;
-                System.out.println("Withdrawal: " + demand+ " successfull");
-
-            } else {
-                System.out.println("Try to Withdraw: " + amount);
-                System.out.println("Insufficient funds. Withdrawal cancelled.");
             }
+
         } finally {
             lock.unlock();
         }
+        if(amount==0)
+            return 1;
+        else
+            return 0;
     }
 
     public static void main(String[] args) {
         ATMWithdrawal account = new ATMWithdrawal();
 
-        Thread withdrawalThread1 = new Thread(() -> account.withdraw(150,200));
-        Thread withdrawalThread2 = new Thread(() -> account.withdraw(1200,5000));
+        Thread withdrawalThread1 = new Thread(() -> account.withdraw(150, 200));
+        Thread withdrawalThread2 = new Thread(() -> account.withdraw(1220,5000));
 
         withdrawalThread1.start();
         withdrawalThread2.start();
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# CashWithdrawal
+Assumptions
+
+User Actions
+-Withdrawal of amount in multiple of 10
+- View balance 
+- fetch balance(to-do)
+
+- ATM has key-value pair of denomination-value
+-concurent hash map
+
+
+Exceptional situtaions
+-ATM not available denomination
+- User balance is not sufficient 
+- User withrawal amount is greater than avaialble balance
+- User amount is not in mulitple of 10
+
